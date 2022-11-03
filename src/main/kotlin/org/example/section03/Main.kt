f563982f30f084ca6496e264ffaaa27c24324014
unidesk <vinicius.nogueira@unidesk.com.br>
2022-11-03 11:19:15 -0300

    Initial commit

---
+++ src/main/kotlin/org/example/section03/Main.kt
@@ -0,0 +1,4 @@
+fun main(args: Array<String>) {
+    val name = if(args.isNotEmpty()) args[0] else "Kotlin World"
+    println("Hello, $name !!")
+}
---
+++ src/main/resources/KotlinTypes.kt
@@ -0,0 +1,22 @@
+fun main(){
+    val minhaFrase = "Eu sou uma String"
+    val minhaPi = 3.14
+    val minhaIdade = 28
+
+    val aString: String
+    val aDouble: Double
+    val aInt: Int
+
+    val myDouble = 1.999
+    val myFloat = 1.9F //Identificar que a variável é float utlizar f ou F ao final da atribuição caso contrário vira Double
+    val myLong = 903430430L //Itentificar que a variável é long utilizar L ao final da atribuição caso contrário vira Int
+    val myInt = 234
+    val myShort: Short = 12
+    val myByte: Byte = 127 //intevalo de -128 até 127
+
+    val aLongNumber = 123_456_789
+    val theSameNumber = 123456789
+
+    val anInt: Int = 7
+    val aLong: Long = anInt.toLong()
+}
